5ab9ae0a64b2f09f9174d694d1c790a699c17976
lordnerd <vinnycfreitas1@gmail.com>
2023-02-28 15:22:30 -0300

    Add an API service and new pages to the site

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Produto } from '../models/produto.model';
+import { ProdutoService } from '../produto.service';
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+	selector: 'app-inicio',
+	templateUrl: './inicio.component.html',
+	styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	public produtos: Produto[] = [];
+	constructor(private _produtoService: ProdutoService) {}
+	ngOnInit(): void {
+		this.listarProdutos();
+	}
+	listarProdutos(): void {
+		this._produtoService.getProdutos().subscribe((retornaProduto) => {
+			this.produtos = retornaProduto.map((item) => {
+				return new Produto(
+					item.id,
+					item.produto,
+					item.descricao,
+					item.foto,
+					item.preco
+				);
+			});
+		});
+	}
 }
---
+++ src/app/restrito/restrito-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GuardGuard } from '../guard.guard';
+import { AtualizaProdutoComponent } from './atualiza-produto/atualiza-produto.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './lista-produto/lista-produto.component';
+import { RestritoComponent } from './restrito.component';
+
+const restritoRoutes: Routes = [
+	{path: 'restrito', component: RestritoComponent, children:[
+		{path: 'cadastro', component: CadastroProdutoComponent, canActivate:[GuardGuard]},
+		{path: 'lista', component: ListaProdutoComponent, canActivate:[GuardGuard]},
+		{path: 'editar/:id', component: AtualizaProdutoComponent, canActivate:[GuardGuard]}
+	]},
+
+	{path: '', redirectTo: '/restrito/lista', pathMatch:'full'}
+]
+
+@NgModule({
+	imports: [RouterModule.forRoot(restritoRoutes)],
+	exports: [RouterModule]
+})
+
+export class RestritoRoutingModule {
+}
